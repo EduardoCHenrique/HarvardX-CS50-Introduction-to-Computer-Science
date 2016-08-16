f4f7c2eef02d30c3836dc81ab0e52678f25f0533
Eduardo Henrique <eduardohenrique@MacBook-Pro-de-Sprinklr.local>
2016-08-16 07:51:25 -0300

    array size and create a new folder to 4th exercise

---
+++ week-2/03-arraySize/arraySize.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+	int n;
+
+	do {
+		printf("Number of people in room: ");
+		n = GetInt();
+	}
+	while (n < 1);
+
+	int ages[n];
+
+	for (int i = 0; i < n ; i++) {
+		printf("Age of person #%i: ", i + 1);
+		ages[i] = GetInt();
+	}
+}
